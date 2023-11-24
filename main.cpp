aa97912f7c9e77b6c946b5c9110fd77e575e39ed
Luis Bueno <luis.bueno@enti.cat>
2023-11-24 10:49:48 +0100

    Primer archivo en CPP

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main (int argc, char** argv){
+	std::cout << "Holi" << std::endl;
+	
+	//std::cout << argc << std::endl;
+
+	if (argc > 1) {
+		std::cout << "Holi" << argv[1] << "!" << std::endl;
+	}
+	else{
+		std::cout << "Introduce un nombre como parametro" << std::endl;
+	}
+
+	return 0;
+}
